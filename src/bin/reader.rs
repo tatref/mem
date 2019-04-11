4631a82ade132649897497c9315cb1d26c63b505
tatref <tatref@github.com>
2019-04-12 00:28:24 +0200

    update

--- src/bin/reader.rs
+++ src/bin/reader.rs
@@ -1,9 +1,10 @@
+#![allow(unused_imports)]
+
 use std::str::FromStr;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::{self,prelude::*};
-use std::fmt;
-use std::time::{Duration, Instant};
+use std::time::Duration;
 use regex;
 
 
@@ -83,7 +84,7 @@ impl FromStr for Device {
 
 
 #[derive(Debug, Clone)]
-struct MappedMemoryRegion {
+struct MappedMemoryRegionMetadata {
     address: Address,
     permissions: Permissions,
     offset: u64,
@@ -92,7 +93,7 @@ struct MappedMemoryRegion {
     pathname: Option<String>,
 }
 
-impl FromStr for MappedMemoryRegion {
+impl FromStr for MappedMemoryRegionMetadata {
     type Err = ();
 
     fn from_str(input: &str) -> Result<Self, Self::Err> {
@@ -105,91 +106,184 @@ impl FromStr for MappedMemoryRegion {
         let inode: u64 = input.next().unwrap().parse().unwrap();
         let pathname: Option<String> = input.next().map(|x| x.to_owned());
 
-        Ok(MappedMemoryRegion{ address, permissions, offset, device, inode, pathname })
+        Ok(MappedMemoryRegionMetadata{ address, permissions, offset, device, inode, pathname })
     }
 }
 
-struct MappedMemoryRegions {
-    maps: Vec<MappedMemoryRegion>,
+struct ProcessLocker {
+    pid: i32,
+    mem_file: File,
 }
 
-impl MappedMemoryRegions {
-    fn from_pid(pid: u32) -> Self {
-        let mut maps_reader = BufReader::new(File::open(format!("/proc/{}/maps", pid))
+impl ProcessLocker {
+    fn lock(pid: i32) -> Result<Self, ()> {
+        if unsafe { libc::kill(pid, libc::SIGSTOP)} != 0 {
+            return Err(());
+        }
+
+        let mem_file = File::open(format!("/proc/{}/mem", pid)).unwrap();
+
+        Ok(ProcessLocker { pid, mem_file })
+    }
+
+    fn get_maps(&self) -> Result<Vec<MappedMemoryRegionMetadata>, ()> {
+        let mut maps_reader = BufReader::new(File::open(format!("/proc/{}/maps", self.pid))
                                              .expect("Unable to open maps file"));
         let mut mem_maps = String::new();
         maps_reader.read_to_string(&mut mem_maps).expect("Unable to read maps file");
 
         let maps: Vec<_> = mem_maps.lines()
             .map(|line| {
-                line.parse::<MappedMemoryRegion>().expect("Unable to parse maps file")
+                line.parse::<MappedMemoryRegionMetadata>().expect("Unable to parse maps file")
             })
         .collect();
 
+        Ok(maps)
+    }
 
-        MappedMemoryRegions { maps }
+    fn get_map_by_name(&self, map_name: &str) -> Result<MappedMemoryRegionMetadata, ()> {
+        let maps = self.get_maps().unwrap();
+        let map = maps
+            .iter()
+            .filter(|map| map.pathname.as_ref().map(|x| x.as_ref()) == Some(map_name))
+            .next()
+            .unwrap()
+            .clone();
+
+        Ok(map)
+    }
+
+    fn get_memory_map(&self, map_name: &str) -> Result<Vec<u8>, ()> {
+        let map = self.get_map_by_name(map_name).unwrap();
+
+        let mem_size = map.address.end - map.address.start;
+        let mut mem_file = self.mem_file.try_clone().unwrap();
+        mem_file.seek(io::SeekFrom::Start(map.address.start)).unwrap();
+        let mut data = vec![0u8; mem_size as usize];
+        mem_file.read_exact(&mut data).unwrap();
+
+        Ok(data)
+    }
+
+    fn stack(&self) -> Result<Vec<u8>, ()> {
+        self.get_memory_map("[stack]")
+    }
+
+    fn heap(&self) -> Result<Vec<u8>, ()> {
+        self.get_memory_map("[heap]")
+    }
+}
+
+impl Drop for ProcessLocker {
+    fn drop(&mut self) {
+        let _ = unsafe { libc::kill(self.pid, libc::SIGCONT)};
     }
 }
 
 
+fn same(a: &[u8], b: &[u8]) -> Vec<bool> {
+    a.iter().zip(b.iter())
+        .map(|(a, b)| a == b)
+        .collect()
+}
+fn increase(a: &[u8], b: &[u8]) -> Vec<bool> {
+    a.iter().zip(b.iter())
+        .map(|(a, b)| a > b)
+        .collect()
+}
+
+
+
+
 fn main() {
-    let pid: u32 = std::env::args().nth(1).expect("Provide pid").parse().expect("Provide pid");
+    let pid: i32 = std::env::args().nth(1).expect("Provide pid").parse().expect("Provide pid");
 
-    let mapped_memory_regions = MappedMemoryRegions::from_pid(pid);
+    let mut last_stack: Option<Vec<u8>> = None;
 
-    let mut mem = File::open(format!("/proc/{}/mem", pid)).unwrap();
+    for i in 0..5
+    {
+        println!("{}", i);
 
+        {
+            let lock = ProcessLocker::lock(pid).unwrap();
+            let stack = lock.stack().unwrap();
 
-    //let re = regex::bytes::Regex::new(r"(?-u)(?P<cstr>[^\x00]+)\x00").unwrap();
-    let re = regex::bytes::Regex::new(r"(?-u)(?P<cstr>[\x20-\x7f]{3,})\x00").unwrap();
+            let needle = 100u32;
+            let needle_size = std::mem::size_of_val(&needle);
 
-    let mut total = 0;
-    for mapped_memory_region in mapped_memory_regions.maps.iter() {
+            if let Some(last) = last_stack {
+                for (a, b) in stack.chunks(needle_size)
+                    .zip(last.chunks(needle_size))
+                    .filter(|(a, b)| a != b) {
 
-        if mapped_memory_region.permissions.r && mapped_memory_region.pathname.as_ref().map(|x| x.as_ref()) != Some("[vvar]") {
-            let instant = Instant::now();
+                    println!("diff: {:?} {:?}", a, b);
+                }
 
-            println!("{:#?}", mapped_memory_region);
-            //println!("mem: {:x}-{:x}", mapped_memory_region.address.start, mapped_memory_region.address.end);
-            println!("size= {}", mapped_memory_region.address.end - mapped_memory_region.address.start);
+            }
 
-            let mem_size = mapped_memory_region.address.end - mapped_memory_region.address.start;
-            total += mem_size;
-            println!("running total={}", total);
-
-            let mut mem = mem.try_clone().unwrap();
-            mem.seek(io::SeekFrom::Start(mapped_memory_region.address.start)).unwrap();
-            //let content: Result<Vec<_>, _>  = mem.take(mem_size).bytes().collect();
-            //let content = content.expect("read mem failed");
-            let mut data = vec![0u8; mem_size as usize];
-            mem.read_exact(&mut data).unwrap();
-
-            //println!("{:?}", data);
-            //println!("content_len={}", content.len());
-
-            let elapsed = instant.elapsed().as_millis() as f32 / 1000.;
-
-            println!("elapsed={:?}", elapsed);
-            println!("");
-
-            let strings: Vec<_> = re.captures_iter(&data)
-                .map(|c| String::from_utf8_lossy(c.name("cstr").unwrap().as_bytes()).to_owned())
-                //.map(|c| c.name("cstr").unwrap().as_bytes())
-                .collect();
-            println!("{:?}", strings);
-
-            //for x in data.windows(5)
-            //    .filter(|x| { x == b"\x00\x01\x02\x03\x04"}) {
-            //        println!("FOUND: {:?} in {:?}", x, mapped_memory_region.pathname);
-            //    }
-
-            //for x in data.windows(5)
-            //    .filter(|x| { x == b"\x10\x11\x12\x13\x14"}) {
-            //        println!("FOUND: {:?} in {:?}", x, mapped_memory_region.pathname);
-            //    }
-
+            last_stack = Some(stack);
         }
+
+        use std::thread::sleep;
+        sleep(Duration::from_millis(2000));
     }
+
+
+
+    //let mut mem = File::open(format!("/proc/{}/mem", pid)).unwrap();
+
+
+    ////let re = regex::bytes::Regex::new(r"(?-u)(?P<cstr>[^\x00]+)\x00").unwrap();
+    //let re = regex::bytes::Regex::new(r"(?-u)(?P<cstr>[\x20-\x7f]{3,})\x00").unwrap();
+    //let re = regex::bytes::Regex::new(r"(?-u)(?P<cstr>New Game)").unwrap();
+
+    //let mut total = 0;
+    //for mapped_memory_region in mapped_memory_regions.maps.iter() {
+
+    //    if mapped_memory_region.permissions.r && mapped_memory_region.pathname.as_ref().map(|x| x.as_ref()) != Some("[vvar]") {
+    //        let instant = Instant::now();
+
+    //        //println!("{:#?}", mapped_memory_region);
+    //        //println!("mem: {:x}-{:x}", mapped_memory_region.address.start, mapped_memory_region.address.end);
+    //        //println!("size= {}", mapped_memory_region.address.end - mapped_memory_region.address.start);
+
+    //        let mem_size = mapped_memory_region.address.end - mapped_memory_region.address.start;
+    //        total += mem_size;
+    //        println!("running total={}", total);
+
+    //        let mut mem = mem.try_clone().unwrap();
+    //        mem.seek(io::SeekFrom::Start(mapped_memory_region.address.start)).unwrap();
+    //        //let content: Result<Vec<_>, _>  = mem.take(mem_size).bytes().collect();
+    //        //let content = content.expect("read mem failed");
+    //        let mut data = vec![0u8; mem_size as usize];
+    //        mem.read_exact(&mut data).unwrap();
+
+    //        //println!("{:?}", data);
+    //        //println!("content_len={}", content.len());
+
+    //        let elapsed = instant.elapsed().as_millis() as f32 / 1000.;
+
+    //        println!("elapsed={:?}", elapsed);
+    //        println!("");
+
+    //        let strings: Vec<_> = re.captures_iter(&data)
+    //            .map(|c| String::from_utf8_lossy(c.name("cstr").unwrap().as_bytes()).to_owned())
+    //            //.map(|c| c.name("cstr").unwrap().as_bytes())
+    //            .collect();
+    //        println!("{:?}", strings);
+
+    //        //for x in data.windows(5)
+    //        //    .filter(|x| { x == b"\x00\x01\x02\x03\x04"}) {
+    //        //        println!("FOUND: {:?} in {:?}", x, mapped_memory_region.pathname);
+    //        //    }
+
+    //        //for x in data.windows(5)
+    //        //    .filter(|x| { x == b"\x10\x11\x12\x13\x14"}) {
+    //        //        println!("FOUND: {:?} in {:?}", x, mapped_memory_region.pathname);
+    //        //    }
+
+    //    }
+    //}
 
 
 
--- src/bin/test2.rs
+++ src/bin/test2.rs
@@ -1,16 +1,20 @@
 use std::io;
+use std::thread::sleep;
+use std::time::Duration;
 
 
 fn main() {
     let pid = std::process::id();
 
-    println!("{}", pid);
+    println!("pid = {}", pid);
 
     let stack_allocated_array: [u8; 5] = *b"\x00\x01\x02\x03\x04";
     let heap_allocated_array: Box<[u8; 5]> = Box::new(*b"\x10\x11\x12\x13\x14");
 
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer).unwrap();
+    for i in 0u32.. {
+        sleep(Duration::from_millis(2_000));
+        println!("{}", i);
+    }
 
     println!("end");
     println!("{:?}", stack_allocated_array);
